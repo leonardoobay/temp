2dfb2e1af551479bdc721f1422ccf3e09083ba8d
leonardoobay <leonardoobay@gmail.com>
2018-11-13 10:51:30 -0800

    MESH &SENSOR

--- Desktop/LEO/AugmentedRealityHydrology-Sensor/arlib/src/main/java/edu/calstatela/jplone/arframework/graphics3d/drawable/Colorable.java
+++
@@ -1,7 +0,0 @@
-package edu.calstatela.jplone.arframework.graphics3d.drawable;
-
-public interface Colorable{
-
-    void setColor(float[] color);
-    void getColor(float[] color);
-}
--- Desktop/LEO/AugmentedRealityHydrology-Sensor/arlib/src/main/java/edu/calstatela/jplone/arframework/graphics3d/drawable/Drawable.java
+++
@@ -1,8 +0,0 @@
-package edu.calstatela.jplone.arframework.graphics3d.drawable;
-
-public interface Drawable {
-
-    void draw(float[] projectionMatrix, float[] viewMatrix, float[] modelMatrix);
-
-    void draw(float[] matrix);
-}
